943d83658f84b07bc0c2ecc5104b089349a5e365
Akhil092004 <akhiljain092004@gmail.com>
2024-10-22 02:33:09 +0530

    added user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -265,7 +265,7 @@ const getCurrentUser = asyncHandler ( async (req,res) =>{
     .json(200,req.user,"Current user fetched succesfully")
 })
 
-const updateAccount = asyncHandler(async (req,res) => {
+const updateAccountDetails = asyncHandler(async (req,res) => {
     const {fullName,email} = req.body
 
     if(!fullName || !email){
@@ -487,7 +487,7 @@ export {
     logoutUser,
     changeCurrentPassword,
     getCurrentUser,
-    updateAccount,
+    updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
     getUserChannelProfile,
